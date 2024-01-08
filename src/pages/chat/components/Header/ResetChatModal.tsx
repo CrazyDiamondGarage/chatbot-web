c6b029089044b8ba1fc0ed65c2bb57d2bcde87c0
endaye <en@endaye.com>
2024-01-09 02:36:09 +0800

    feat(1229): only add ui comps, and show modal to start a new chat; ⚠lack of network APIs

---
+++ src/pages/chat/components/Header/ResetChatModal.tsx
@@ -0,0 +1,38 @@
+import { Button, Modal } from 'antd'
+
+type Props = {
+  characterName: string
+  open: boolean
+  onCancle: () => void
+  onConfirm: () => void
+}
+
+export function ResetChatModal({
+  characterName,
+  open,
+  onCancle,
+  onConfirm,
+}: Props) {
+  return (
+    <Modal open={open} footer={null} closable={false} width={416} centered>
+      <div className="my-2 text-center">
+        <div className="text-18px font-600">
+          Start a new chat with {characterName} ?
+        </div>
+        <div className="mt-6">
+          <Button size="large" onClick={onCancle}>
+            Cancel
+          </Button>
+          <Button
+            type="primary"
+            size="large"
+            className="ml-3"
+            onClick={onConfirm}
+          >
+            Confirm
+          </Button>
+        </div>
+      </div>
+    </Modal>
+  )
+}
